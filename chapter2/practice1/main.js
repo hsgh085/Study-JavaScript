26f91d4214624b51a7cf95233f46f7437e7ebbd2
hsgh085 <hsyu0917@gmail.com>
2023-12-27 22:44:48 +0900

    Practice Web APIs

---
+++ chapter2/practice1/main.js
@@ -0,0 +1,20 @@
+addEventListener("load", () => {
+  const vertical = document.querySelector(".vertical");
+  const horizontal = document.querySelector(".horizontal");
+  const target = document.querySelector(".target");
+  const tag = document.querySelector(".tag");
+  const targetRect = target.getBoundingClientRect();
+  const targetHalfWidth = targetRect.width / 2;
+  const targetHalfHeight = targetRect.height / 2;
+
+  window.addEventListener("mousemove", (e) => {
+    const x = e.clientX;
+    const y = e.clientY;
+    vertical.style.transform = `translateX(${x}px)`;
+    horizontal.style.transform = `translateY(${y}px)`;
+
+    target.style.transform = `translate(${x - targetHalfWidth}px, ${y - targetHalfHeight}px)`;
+    tag.style.transform = `translate(${x}px, ${y}px)`;
+    tag.innerHTML = `${x}px, ${y}px`;
+  });
+});
